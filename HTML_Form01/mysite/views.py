0ac371a7be37a79fb1c03cecdb8eea4f4d749f1e
changefor <changefor@cht.com.tw>
2023-05-24 09:40:14 +0800

    add form template

--- HTML_Form01/mysite/views.py
+++ HTML_Form01/mysite/views.py
@@ -3,14 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def get_example(request):
     try:
+        fcolor = request.GET['fcolor']
+        fmovie = request.GET['fmovie']
         urid = request.GET['user_id']
         urpass = request.GET['user_pass']
+        byear = request.GET['byear']
     except:
         urid = None
+
     
     if urid != None and urpass == '12345':
         verified = True
     else:
         verifeid = False
-
+    
+    years = range(1960,2023+1)
     return render(request, 'get_example.html', locals())
